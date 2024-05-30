e68a53e3815a193186e1dfac765e412187d5ce4a
Hemil Kaklotar <hemilkaklotar@gmail.com>
2024-05-30 17:49:53 +0530

    Added: Starship terminal for visual

--- Powershell/Microsoft.PowerShell_profile.ps1
+++ Powershell/Microsoft.PowerShell_profile.ps1
@@ -1,11 +1,18 @@
+$ENV:STARSHIP_CONFIG = "$HOME\dotfiles\config\starship\starship.toml"
+
 oh-my-posh init pwsh | Invoke-Expression
 Set-Alias -Name vim -Value nvim
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\robbyrussell.omp.json" | Invoke-Expression
 Import-Module -Name Terminal-Icons
 # $env:VIRTUAL_ENV_DISABLE_PROMPT = 1
 
+# Set up the starship prompt
+Invoke-Expression (&starship init powershell)
+
+# Set up zoxide
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
 
+# Nvim with fzf finder
 function Invoke-NvimFzf {
     $directory = Get-Location
     $previewCommand = 'cat {}'
