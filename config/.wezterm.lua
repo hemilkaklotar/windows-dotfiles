92115948d10b03fd88aadddc9ebaf83d8a9df9fc
Hemil Kaklotar <83568973+hemilkaklotar@users.noreply.github.com>
2024-10-17 09:50:59 +0530

    Update: config update for wezterm and starship

--- config/.wezterm.lua
+++ config/.wezterm.lua
@@ -1,43 +1,59 @@
 local wezterm = require("wezterm")
 local act = wezterm.action
 
-wezterm.on("update-right-status", function(window, pane)
-	-- set the right status to the active workspace with name of current workspace
-	window:set_right_status(window:active_workspace())
-end)
-
--- Configuration object
 local config = {}
 
--- General settings
 config.audible_bell = "Disabled"
-config.color_scheme = "Rosé Pine (Gogh)"
--- config.color_scheme = "Catppuccin Mocha"
-config.font_size = 14
-config.front_end = 'OpenGL'
-config.freetype_load_target = 'Light'
-config.freetype_load_flags = 'NO_HINTING'
-config.freetype_render_target = 'HorizontalLcd'
+config.color_scheme = "Navy and Ivory (terminal.sexy)"
+config.font_size = 16
+config.front_end = "OpenGL"
+config.freetype_load_target = "Light"
+config.freetype_load_flags = "NO_HINTING"
+config.freetype_render_target = "HorizontalLcd"
 config.detect_password_input = true
--- Window settings
 config.window_decorations = "RESIZE"
-config.window_padding = {
-	left = 20,
-	right = 20,
-	top = 10,
-	bottom = 10,
-}
--- config.enable_tab_bar = false -- Enable the tab bar
 config.window_close_confirmation = "NeverPrompt"
--- config.window_background_opacity = 0.5
--- config.win32_system_backdrop = "Mica"
 config.warn_about_missing_glyphs = false
--- Font settings
 config.font = wezterm.font_with_fallback({
-	"0xProto Nerd Font",
+	"RobotoMono Nerd Font Mono",
+	"0xProto Nerd Font Mono",
 	"JetBrainsMono Nerd Font",
 	"Noto Color Emoji",
 })
+
+wezterm.on("toggle-background", function(window, pane)
+	local overrides = window:get_config_overrides() or {}
+	if wezterm.GLOBAL.background_empty == true then
+		overrides.background = {
+			{
+				source = {
+					File = wezterm.GLOBAL.background,
+				},
+				repeat_x = "NoRepeat",
+				vertical_align = "Bottom",
+				hsb = { brightness = 0.05, hue = 1, saturation = 1 },
+				opacity = 1,
+			},
+		}
+		wezterm.GLOBAL.background_empty = false
+	else
+		wezterm.GLOBAL.background_empty = true
+		overrides.background = {}
+	end
+	window:set_config_overrides(overrides)
+end)
+
+wezterm.on("toggle-opacity", function(window, pane)
+	local overrides = window:get_config_overrides() or {}
+	if not overrides.window_background_opacity then
+		overrides.window_background_opacity = 1
+	else
+		overrides.window_background_opacity = nil
+	end
+	window:set_config_overrides(overrides)
+end)
+
+config.window_background_opacity = 0.9
 
 local function file_exists(name)
 	local f = io.open(name, "r")
@@ -49,7 +65,6 @@ local function file_exists(name)
 	end
 end
 
--- WSL domain settings
 local wsl_domains = wezterm.default_wsl_domains()
 for idx, dom in ipairs(wsl_domains) do
 	if dom.name == "WSL:Ubuntu-24.04" then
@@ -59,7 +74,6 @@ end
 config.wsl_domains = wsl_domains
 
 local default_prog = nil
--- Default program settings
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
 	local useWslUbuntu = false
 	if useWslUbuntu then
@@ -77,16 +91,13 @@ end
 
 config.default_prog = default_prog
 
--- Cursor settings
 config.default_cursor_style = "BlinkingBar"
 config.cursor_blink_rate = 600
 config.cursor_blink_ease_out = "Linear"
 
--- Keybindings
 config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
 config.disable_default_key_bindings = true
 config.keys = {
-	-- Leader key actions
 	{ key = "a", mods = "CTRL|LEADER", action = wezterm.action({ SendString = "\x01" }) },
 	{ key = "-", mods = "LEADER", action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
 	{ key = "\\", mods = "LEADER", action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
@@ -111,8 +122,8 @@ config.keys = {
 	{ key = "9", mods = "LEADER", action = wezterm.action({ ActivateTab = 8 }) },
 	{ key = "&", mods = "LEADER|SHIFT", action = wezterm.action({ CloseCurrentTab = { confirm = true } }) },
 	{ key = "x", mods = "LEADER", action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
-
-	-- General actions
+	{ key = "o", mods = "CTRL|META", action = wezterm.action.EmitEvent("toggle-opacity") },
+	{ key = "m", mods = "CTRL|META", action = wezterm.action.EmitEvent("toggle-background") },
 	{ key = "n", mods = "SHIFT|CTRL", action = "ToggleFullScreen" },
 	{ key = "v", mods = "SHIFT|CTRL", action = wezterm.action.PasteFrom("Clipboard") },
 	{ key = "c", mods = "SHIFT|CTRL", action = wezterm.action.CopyTo("Clipboard") },
@@ -120,19 +131,40 @@ config.keys = {
 	{ key = "-", mods = "SHIFT|CTRL", action = "DecreaseFontSize" },
 	{ key = "_", mods = "SHIFT|CTRL", action = "DecreaseFontSize" },
 	{ key = "0", mods = "SHIFT|CTRL", action = "ResetFontSize" },
-
-	-- Custom key mappings
 	{
 		key = "j",
 		mods = "CTRL|SHIFT",
 		action = act.ActivatePaneDirection("Next"),
 	},
 
-	-- Close current tab
 	{ key = "w", mods = "CTRL", action = wezterm.action.CloseCurrentTab({ confirm = true }) },
+	{
+		key = "y",
+		mods = "CTRL|SHIFT",
+		action = act.SwitchToWorkspace({
+			name = "default",
+		}),
+	},
+	{
+		key = "u",
+		mods = "CTRL|SHIFT",
+		action = act.SwitchToWorkspace({
+			name = "work",
+			spawn = {
+				args = { "top" },
+			},
+		}),
+	},
+	{ key = "v", mods = "LEADER", action = act.SwitchToWorkspace },
+	{
+		key = "w",
+		mods = "LEADER",
+		action = act.ShowLauncherArgs({
+			flags = "FUZZY|WORKSPACES",
+		}),
+	},
 }
 
--- Launch menu
 config.launch_menu = {
 	{ label = "bash", args = { "bash", "-l" } },
 	{ label = "zsh", args = { "zsh", "-l" } },
@@ -141,22 +173,18 @@ config.launch_menu = {
 local bar = wezterm.plugin.require("https://github.com/adriankarlen/bar.wezterm")
 bar.apply_to_config(config)
 
-bar.apply_to_config(
-  config,
-  {
-    modules = {
-      spotify = {
-        enabled = true,
-      },
-    },
-  }
-)
+bar.apply_to_config(config, {
+	modules = {
+		spotify = {
+			enabled = true,
+		},
+	},
+})
 
 local tabbar = config.colors.tab_bar
 
 tabbar.background = "transparent"
 tabbar.active_tab.bg_color = "#26233a"
-tabbar.inactive_tab.bg_color = "transparent" 
-tabbar.active_tab.fg_color = "#fff" 
--- Return the configuration to wezterm
+tabbar.inactive_tab.bg_color = "transparent"
+tabbar.active_tab.fg_color = "#fff"
 return config
